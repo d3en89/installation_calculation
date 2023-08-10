7a88167dba2860fcf6c1743e0d5e7d23f8e335b6
d3en89 <stdenix889@yandex.ru>
2023-08-11 01:11:20 +0300

    added js function update sum cells automatically

--- calc/static/js/calc.js
+++ calc/static/js/calc.js
@@ -1,4 +1,11 @@
 // this code  enter sum when use button
+  var input = document.body.children[0];
+
+    input.oninput = function() {
+    document.getElementById('result').innerHTML = input.value;
+};
+
+
 var sumBtn = document.querySelector('#plus');
 sumBtn.onclick = function() {
   var table = document.querySelector('#pricelist');
@@ -10,6 +17,13 @@ sumBtn.onclick = function() {
     total.innerHTML = sum;
   }
 }
+
+function update_cell(id, count) {
+  price =  document.getElementById("pricelist").rows[id].cells[2].textContent
+  data = Number(price) * Number(count)
+  document.getElementById("pricelist").rows[id].cells[6].textContent = data;
+}
+
 
 //
 //var table = document.querySelector('#pricelist');
