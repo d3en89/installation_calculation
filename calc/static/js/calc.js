55378f51ce5422a63753501426c4c0e548fadbce
d3en89 <stdenix889@yandex.ru>
2023-08-13 15:14:07 +0300

    add autorefresh total sum

--- calc/static/js/calc.js
+++ calc/static/js/calc.js
@@ -1,37 +1,35 @@
-// this code  enter sum when use button
-  var input = document.body.children[0];
-
-    input.oninput = function() {
-    document.getElementById('result').innerHTML = input.value;
-};
-
-
-var sumBtn = document.querySelector('#plus');
-sumBtn.onclick = function() {
-  var table = document.querySelector('#pricelist');
-  var tds = table.querySelectorAll('input');
-  var quantity = table.querySelectorAll('span')
-  var sum = 0;
-  for (var i = 0; i < tds.length; i++) {
-    sum += Number(tds[i].value) * Number(quantity[i].innerHTML) ;
-    total.innerHTML = sum;
-  }
-}
-
 function update_cell(id, count) {
-  price =  document.getElementById("pricelist").rows[id].cells[2].textContent
-  data = Number(price) * Number(count)
+  // update cost sum line if  quantity edit
+  price =  document.getElementById("pricelist").rows[id].cells[2].textContent;
+  data = Number(price) * Number(count);
   document.getElementById("pricelist").rows[id].cells[6].textContent = data;
+  
+  // when update line this func  update tota_cost
+  var table = document.querySelector('#pricelist');
+  var quantity = table.querySelectorAll('#sum_work');
+  total_cost = 0;
+  for (var i = 0; i < quantity.length; i++) {
+      total_cost += Number(quantity[i].textContent);
+  }
+  total.innerHTML = total_cost;
 }
 
 
-//
-//var table = document.querySelector('#pricelist');
-//var tds = table.querySelectorAll('input');
-//var quantity = table.querySelectorAll('span')
-//var sum = 0;
-//for (var i = 0; i < tds.length - 1; i++) {
-//	    sum += Number(tds[i].value) * Number(quantity[i].innerHTML) ;
-//    total.innerHTML = sum;
-//}
+//  if you whant use button this code for you
 
+//start
+
+// var sumBtn = document.querySelector('#plus');
+
+// sumBtn.onclick = function() {
+//   var table = document.querySelector('#pricelist');
+//   var tds = table.querySelectorAll('input');
+//   var quantity = table.querySelectorAll('span');
+//   var sum = 0;
+//   for (var i = 0; i < tds.length; i++) {
+//     sum += Number(tds[i].value) * Number(quantity[i].innerHTML) ;
+//     total.innerHTML = sum;
+//   }
+// }
+
+//end
