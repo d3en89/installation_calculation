d48076563783005ce7cbf56458c8c11f38625b98
d3en89 <stdenix889@yandex.ru>
2023-08-16 20:43:24 +0300

    Added button to download template file, the file is generated when using the download

--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request, HttpRequest
-from core.settings import MEDIA_ROOT
+from core.settings import MEDIA_ROOT, BASE_DIR
+import mimetypes
+from openpyxl import Workbook
 
 from calc.static.script.get_data_db import get_price
 from calc.static.script import get_data_file
@@ -9,10 +11,10 @@ from calc.models import File_upload
 # Create your views here.
 
 def index(request : HttpRequest):
-    #return HttpResponse("Hello, world. You're at the calc index.")
     list_price = {
         'all_works' : get_price()
     }
+
     match request.POST:
         case {'Send': 'Отправить'}:
             file = File_upload(name=str(request.FILES.get('send_files')),
@@ -24,3 +26,30 @@ def index(request : HttpRequest):
                 get_data_file.main(MEDIA_ROOT, str(file.file_point))
 
     return render(request, 'calc/index.html', list_price)
+
+def download_file(request):
+
+        filename = 'temp_price.xlsx'
+        filepath = f'{BASE_DIR}/calc/static/template/{filename}'
+        mime_type, _ = mimetypes.guess_type(filepath)
+        response = HttpResponse(content_type=mime_type)
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.title = 'Price'
+        columns = [
+            'Индекс',
+            'Название работ',
+            'Количество',
+            'Ед. измерения',
+            'Цена',
+            'Валюта',
+        ]
+        row_num = 1
+
+        for col_num, column_title in enumerate(columns, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+
+        workbook.save(response)
+        return response
--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='calc'),
+    path('download_file', views.download_file, name='download_file')
+    #path('download_file', views.download_file, name='download_file')
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
