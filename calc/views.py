6c6ef45c49f75f6c27f1126c97529a2a08f54537
d3en89 <stdenix889@yandex.ru>
2023-08-16 17:51:11 +0300

    only reload page if no file is selected  to upload

--- calc/views.py
+++ calc/views.py
@@ -14,11 +14,13 @@ def index(request : HttpRequest):
         'all_works' : get_price()
     }
     match request.POST:
-
         case {'Send': 'Отправить'}:
             file = File_upload(name=str(request.FILES.get('send_files')),
                                file_point=request.FILES.get('send_files'))
-            file.save()
-            get_data_file.main(MEDIA_ROOT, str(file.file_point))
+            if file.name == "None":
+                return render(request, 'calc/index.html', list_price)
+            else:
+                file.save()
+                get_data_file.main(MEDIA_ROOT, str(file.file_point))
 
     return render(request, 'calc/index.html', list_price)
