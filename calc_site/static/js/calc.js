3aa02f0bf05c714cf9716d728eb31c1492c290dc
d3en89 <stdenix889@yandex.ru>
2023-09-19 19:54:16 +0300

    Added new function update_examp_total_sum

--- calc_site/static/js/calc.js
+++ calc_site/static/js/calc.js
@@ -14,11 +14,22 @@ function update_cell(id, count) {
   total.innerHTML = total_cost;
 }
 
+function update_examp_total_sum() {
+  var examp_total_cost = $('.calc_examp_cost_span_1');
+  var column_cost =   $('#calc_examp .calc_examp_cost')
+  var sum = 0
+  for (let i = 0; i < column_cost.length; i++) {
+    sum += Number(column_cost[i].innerText)
+  }
+  examp_total_cost.text(sum)
+}
+
 function update_calc_examp(id_var, name_var) {
   var level_work = $('input[name="complexity"]:checked').val();
   var quantity  = $('#calc_examp span#' + id_var + '[name="enter"]');
   var cost  = $('#calc_examp span#' + id_var + '[name="cost"]');
   var price_cost  = $('#calc_examp span#' + id_var + '[name="price_cost"]');
+
 
   if(name_var == "increase"){
     quantity.text(Number(quantity.text()) + 1);
@@ -29,6 +40,9 @@ function update_calc_examp(id_var, name_var) {
     quantity.text(Number(quantity.text()) - 1);
     cost.text(Number(quantity.text()) * Number(price_cost.text()) + (Number(quantity.text()) * Number(level_work) )  );
   }
+
+  update_examp_total_sum()
+
 }
 
 function update_calc_examp_def(id_var, name_var) {
@@ -46,4 +60,9 @@ function update_calc_examp_def(id_var, name_var) {
     quantity.text(Number(quantity.text()) - 1);
     cost.text(Number(quantity.text()) * Number(price_cost.text()));
   }
+
+  update_examp_total_sum()
+
 }
+
+
